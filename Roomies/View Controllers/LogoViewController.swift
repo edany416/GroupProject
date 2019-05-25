ef74c2a3dcc52c9578b9225508c6c291e7cfec80
edanyachdav <edan.yachdav@gmail.com>
2019-05-25 00:12:03 -0700

    Launch screen created and activity indicator added to Logo screen for when loading signed in user's data

--- Roomies/View Controllers/LogoViewController.swift
+++ Roomies/View Controllers/LogoViewController.swift
@@ -7,35 +7,33 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 class LogoViewController: UIViewController {
 
     @IBOutlet weak var logInButton: RoundedButton!
     @IBOutlet weak var signUpButton: RoundedButton!
+    @IBOutlet weak var activityIndicator: NVActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        signUpButton.backgroundColor = UIColor.clear
         
+        signUpButton.backgroundColor = UIColor.clear
         logInButton.backgroundColor = UIColor.white
         
-        // Do any additional setup after loading the view.
+        activityIndicator.type = .ballBeat
+        activityIndicator.color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if FirebaseManager.instance.userIsLoggedIn {
+            activityIndicator.startAnimating()
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
